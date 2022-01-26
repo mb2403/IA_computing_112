3f44bb102dbf4770157d30e8d874c4ff712bf84c
Lewis Clark <lewisclarkan@gmail.com>
2022-01-26 17:03:44 +0000

    Alteration to Task1F

--- Task1F.py
+++ Task1F.py
@@ -9,7 +9,7 @@ def run():
     stations = build_station_list()
     inconsistent_stations = inconsistent_typical_range_stations(stations)
     inconsistent_stations.sort()
-    print(inconsistent_stations)
+    print("The stations with inconsistent typical range data are: {}".format(inconsistent_stations))
     
 if __name__ == "__main__":
     print("*** Task 1F: CUED Part IA Flood Warning System ***")
