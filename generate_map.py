9a52d9b98dffe17e42cc5c026ed113d776f26dca
Lewis Clark <lewisclarkan@gmail.com>
2022-01-27 22:46:24 +0000

    Changes to generate_map

--- generate_map.py
+++ generate_map.py
@@ -9,13 +9,11 @@ import plotly.express as px
 
 import datetime
 
-from floodsystem.datafetcher import fetch_measure_levels
-from floodsystem.analysis import polyfit
-from floodsystem.plot import plot_water_level_with_fit
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 
-def run():
+#def generate_map(stations, colour = "yellow", tol=0, ):
+def run(): 
     stations = build_station_list()
     update_water_levels(stations)
 
@@ -25,22 +23,23 @@ def run():
     relative_levels = []
     lats=[]
     longs = []
-    print(stations[0])
+    towns = []
 
     for item in stations:
         station_names.append(item.name)
         locations.append(item.coord)
         relative_levels.append(item.relative_water_level())
+        towns.append(item.town)
+
 
     for item in locations:
         lats.append(item[0])
         longs.append(item[1])
 
-    d = {'Station Name': station_names, 'Latitude' : lats, 'Longitude' : longs, 'e': relative_levels }
+    d = {'Station Name': station_names, 'Latitude' : lats, 'Longitude' : longs, 'Relative Level': relative_levels, 'Town': towns }
     stations_data_frame = pd.DataFrame(data=d)
-    print(stations_data_frame)
 
-    fig = px.scatter_mapbox(stations_data_frame, lat = 'Latitude', lon = 'Longitude', hover_name = 'Station Name', hover_data = 'e')
+    fig = px.scatter_mapbox(stations_data_frame, lat = 'Latitude', lon = 'Longitude', hover_name = 'Station Name', hover_data = ['Relative Level', 'Town'], color_discrete_sequence=["black"],)
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig.show()
