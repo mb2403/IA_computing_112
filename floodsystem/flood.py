2593780c2c49c72c556142e421d8c5391077c7a4
MaxB <max.bowler1106@gmail.com>
2022-01-30 17:11:36 +0000

    module for returning N highest risk stations

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -19,3 +19,19 @@ def stations_level_over_threshold(stations,tol):                                
     
     return sorted_by_key(combined_list,1)                                                                   #returns a list sorted by relative level
 
+def stations_highest_rel_level(stations, N):
+    stations_list = []
+    relatives_list = []
+    
+    for item in stations:
+        if (item.typical_range_consistent() == False) or (item.relative_water_level() == None):             #checks if data is valid
+            pass
+        else:                                                           #if data > tol then add to list.
+            stations_list.append(item)
+            relatives_list.append(item.relative_water_level())
+            
+    combined_list = zip(stations_list,relatives_list)                                                       #combines two lists into a list of tuples
+    
+    combined_list = sorted_by_key(combined_list, 1, reverse=True)
+
+    return combined_list[:N]
