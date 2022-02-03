10168a0f9bf1aab8871a9712f0d9b7f63ea9f608
MaxB <max.bowler1106@gmail.com>
2022-02-03 13:35:08 +0000

    Not currently working, would help to get a second pari of eyes

--- Task2E.py
+++ Task2E.py
@@ -1,7 +1,7 @@
 # Max Bowler Jan 2022
 # Task 2E
 #
-
+#%%
 import datetime
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.analysis import polyfit
@@ -9,6 +9,7 @@ from floodsystem.plot import plot_water_levels
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
 import numpy as np
+import matplotlib.pyplot as  plot
 
 def run():
     stations = build_station_list()
@@ -28,8 +29,8 @@ def run():
     for i in range(len(data_list)):
         dates[i], levels[i] = fetch_measure_levels(data_list[i].measure_id, dt = datetime.timedelta(days = dt))
 
-
-    plot_water_levels(data_list[i], dates[i], levels[i])
+        plot_water_levels(data_list[i], dates[i], levels[i])
+    
 
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
