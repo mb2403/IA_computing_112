50fc6d4660cadf6478fd0aceabb0f2d7eab3c397
MaxB <max.bowler1106@gmail.com>
2022-02-05 15:03:45 +0000

    Flood submodule test complete

---
+++ test_flood.py
@@ -0,0 +1,37 @@
+#  TEST for  flood submodule
+#Max B Feb 2022
+#Testing subroutines in the "Flood" submodule
+
+from cgi import test
+from  floodsystem.station import MonitoringStation
+from floodsystem.stationdata import build_station_list, update_water_levels
+import floodsystem.flood as flood
+
+
+stations = build_station_list()
+update_water_levels(stations)
+#Need to create  an object to test as an input
+
+sacrificial_offering = [MonitoringStation(
+                station_id=1,
+                measure_id=10,
+                label='sacrificial_offering_1',
+                coord=(float(0.1), float(0.1)),
+                typical_range= (1.2, 1.5),
+                river='river_offering',
+                town='town_offering'),
+                MonitoringStation(
+                station_id=2,
+                measure_id=20,
+                label='sacrificial_offering_2',
+                coord=(float(50), float(0)),
+                typical_range= (15, 2),
+                river='river_offering',
+                town='town_offering')]
+
+
+def test_stations_highest_rel_level():
+    sacrificial_offering[0].latest_level = 1.4
+    sacrificial_offering[1].latest_level = 255
+    assert flood.stations_highest_rel_level(sacrificial_offering,  1) == [('sacrificial_offering_1', -0.10000000000000009)]
+    
