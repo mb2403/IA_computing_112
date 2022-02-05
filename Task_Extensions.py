72796d1193abc1a77dcd2955d02ea4cc96e32899
Lewis Clark <lewisclarkan@gmail.com>
2022-02-05 20:15:22 +0000

    e

--- Task_Extensions.py
+++ Task_Extensions.py
@@ -1,12 +1,13 @@
 #LC and MB 2022
 
-from floodsystem.generate_map import generate_map
+from floodsystem.generate_map import generate_layer_map, generate_bubble_map
 from floodsystem.stationdata import build_station_list, update_water_levels
 
 def run():
     stations = build_station_list()
     update_water_levels(stations)
-    generate_map(stations)              
+    generate_layer_map(stations)  
+    generate_bubble_map(stations)            
     
 if __name__ == "__main__":
     print("***Extensions: CUED Part IA Flood Warning System ***")
