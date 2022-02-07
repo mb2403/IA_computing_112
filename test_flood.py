323498d66ca63b01c81fc0381d05522957fe1f1f
Lewis Clark <lewisclarkan@gmail.com>
2022-02-07 14:33:28 +0000

    test_flood.pp

--- test_flood.py
+++ test_flood.py
@@ -12,27 +12,30 @@ stations = build_station_list()
 update_water_levels(stations)
 #Need to create  an object to test as an input
 
-sacrificial_offering = [MonitoringStation(
+sacrificial_offering_1 = MonitoringStation(
                 station_id=1,
                 measure_id=10,
                 label='sacrificial_offering_1',
                 coord=(float(0.1), float(0.1)),
                 typical_range= (1.2, 1.5),
                 river='river_offering',
-                town='town_offering'),
-                MonitoringStation(
+                town='town_offering')
+
+sacrificial_offering_2 = MonitoringStation(
                 station_id=2,
                 measure_id=20,
                 label='sacrificial_offering_2',
                 coord=(float(50), float(0)),
                 typical_range= (15, 2),
                 river='river_offering',
-                town='town_offering')]
+                town='town_offering')
+
+
+sacrificial_offering = [sacrificial_offering_1, sacrificial_offering_2]
 
 
 def test_stations_highest_rel_level():
     sacrificial_offering[0].latest_level = 1.4
     sacrificial_offering[1].latest_level = 255
-    correct_output =  ("Station name:     sacrificial_offering_1\n   id:            1\n   measure id:    10\n   coordinate:    (0.1, 0.1)\n   town:          town_offering\n   river:         river_offering\n   typical range: (1.2, 1.5)", 0.6666666666666664)
-    assert flood.stations_highest_rel_level(sacrificial_offering,  1) == correct_output
+    assert flood.stations_highest_rel_level(sacrificial_offering,  1) == [(sacrificial_offering_1,0.6666666666666664)]
     
