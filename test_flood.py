5bb29fa369e174f4aa98cd45d0910b3bc3fbe4ca
Lewis Clark <lewisclarkan@gmail.com>
2022-02-10 14:38:46 +0000

    test_flood

--- test_flood.py
+++ test_flood.py
@@ -39,5 +39,5 @@ def test_stations_highest_rel_level():
     
 def test_stations_over_threshold():
     sacrificial_offering_1.latest_level = 10
-    sacrificial_offering_2.latest_level = 20
-    assert stations_level_over_threshold(sacrificial_offering, 15) == sacrificial_offering_1
+    sacrificial_offering_2.latest_level = 3
+    assert stations_level_over_threshold(sacrificial_offering, 0.6) == sacrificial_offering[0]
